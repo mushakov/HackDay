d9cbf8e1bd35ef594e246944e5857c4e6664ca24
mushakov <melissa.ushakov@gmail.com>
2015-07-18 20:31:43 -0500

    All the things

---
+++ Tutorials/src/helloworld.js
@@ -0,0 +1,4 @@
+React.render(
+  <h1>Happy Birthday Dima!</h1>,
+  document.getElementById('example')
+);
---
+++ react-0.13.3/build/helloworld.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement("h1", null, "Happy Birthday Dima!"),
+  document.getElementById('example')
+);
